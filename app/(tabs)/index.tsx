5e90f7d24a0edb26bf3eb5d5a6de14b8588609b3
in kang <ken940816@gmail.com>
2024-12-22 17:32:34 +0900

    사진 선택, 미리보기 적용

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,64 @@
 import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 // 이미지 가져오기
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+    const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+    /**
+     * 사진첩에서 사진 픽 해오기
+     */
+    const pickImageAsync = async () => {
+        // 다양한 옵션을 지정하기 위한 객체를 받습니다.
+        // 이 객체는 메소드 ImagePickerOptions를 호출할 때 전달하는 객체
+        /** 아래와 같은 결과를 반환
+         * {
+            "assets": [
+              {
+                "assetId": "99D53A1F-FEEF-40E1-8BB3-7DD55A43C8B7/L0/001",
+                "base64": null,
+                "duration": null,
+                "exif": null,
+                "fileName": "IMG_0004.JPG",
+                "fileSize": 2548364,
+                "height": 1669,
+                "mimeType": "image/jpeg",
+                "type": "image",
+                "uri": "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252FStickerSmash-13f21121-fc9d-4ec6-bf89-bf7d6165eb69/ImagePicker/ea574eaa-f332-44a7-85b7-99704c22b402.jpeg",
+                "width": 1668
+              }
+            ],
+            "canceled": false
+          }
+         */
+        let result = await ImagePicker.launchImageLibraryAsync({
+            // 미디어 타입
+            mediaTypes: ["images"],
+            // true로 설정되어 있는 경우, 사용자는 android, ios에서
+            // 이미지를 자를 수 있음
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri);
+        } else {
+            alert("You did not select any image.");
+        }
+    };
+
     return (
         <View style={styles.container}>
             <View style={styles.imageContainer}>
-                <ImageViewer imgSource={PlaceholderImage} />
+                <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
             </View>
             <View style={styles.footerContainer}>
-                <Button label="Choose a photo" theme="primary" />
+                <Button label="Choose a photo" theme="primary" onPress={pickImageAsync} />
                 <Button label="Use this photo" />
             </View>
         </View>
@@ -30,7 +76,7 @@ const styles = StyleSheet.create({
         paddingTop: 28,
     },
     footerContainer: {
-        flex: 1 / 50,
+        flex: 1 / 3,
         alignItems: "center",
     },
 });
--- components/Button.tsx
+++ components/Button.tsx
@@ -4,19 +4,17 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 type Props = {
     label: string;
     theme?: "primary";
+    onPress?: () => void;
 };
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
     if (theme === "primary") {
         return (
             <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
                 {/* React Native에는 터치 이벤트를 처리하기 위한 몇 가지 다른 구성 요소가 포함되어 있지만 
                 <Pressable>유연성 때문에 권장됩니다. 단일 탭, 긴 누름을 감지하고, 
                 버튼을 눌렀다 놓으면 별도의 이벤트를 트리거하는 등의 작업을 수행할 수 있습니다. */}
-                <Pressable
-                    style={[styles.button, { backgroundColor: "#fff" }]}
-                    onPress={() => alert("You pressed a button.")}
-                >
+                <Pressable style={[styles.button, { backgroundColor: "#fff" }]} onPress={onPress}>
                     <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
                     <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
                 </Pressable>
